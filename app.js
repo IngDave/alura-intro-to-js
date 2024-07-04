d7e4b6a5e4643bb631f927b9d583cfb79653e0d8
david tejada <daves.dev@outlook.com>
2024-07-04 15:36:08 -0500

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let numeroAdivinar = prompt("Ingresa un numero del 1 - 10");
 console.log(numeroAdivinar);
 
 if (numeroAdivinar == numeroSecreto) {
-  alert("Has acertado, numero secreto: " + numeroSecreto) + alert("¡Game Over!");
-} else {
-  alert("Intenta nuevamente") + alert("El numero secreto era: " + numeroSecreto );
+  alert(`Has acertado, numero secreto: ${numeroSecreto}`);
+} else { 
+  alert("Intenta nuevamente") + alert(`El numero secreto era: ${numeroSecreto}`);
 }
--- desafioHoraDePracticar5.js
+++ desafioHoraDePracticar5.js
@@ -1,2 +1,9 @@
 /*Pide al usuario que ingrese su nombre mediante un prompt. Luego, 
 muestra una alerta de bienvenida usando ese nombre.*/
+
+function mostrarAlert(){
+    let msj = prompt("Cual es tu nombre: ")
+    return msj;
+}
+
+alert("Hola!"+" "+mostrarAlert() )
