95d768820bce124add443c933eacbca4510240ad
david tejada <daves.dev@outlook.com>
2024-07-05 14:07:18 -0500

    update

--- desafioHoraDePracticar5.js
+++ desafioHoraDePracticar5.js
@@ -2,8 +2,8 @@
 muestra una alerta de bienvenida usando ese nombre.*/
 
 function mostrarAlert(){
-    let msj = prompt("Cual es tu nombre: ")
+    let msj = prompt("Cual es tu nombre: ");
     return msj;
 }
 
-alert("Hola!"+" "+mostrarAlert() )
+alert("Hola!"+" "+mostrarAlert());
