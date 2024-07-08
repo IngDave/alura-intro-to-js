a47c28c55f8bcc6eb5704f3d69df129513d7e83a
david tejada <daves.dev@outlook.com>
2024-07-07 23:57:27 -0500

    update final game

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-let numeroSecreto = Math.floor(Math.random()*10+1);
+function numeroDinamico(max, min) {
+  max = prompt("Ingrese un rango maximo: ");
+  min = 1;
+  return Math.floor(Math.random() * max + 1);
+}
+let numeroSecreto = numeroDinamico();
 let numeroAdivinar = 0;
 let intentos = 1;
-//loop para dinamimizar el juego
+let personalizarIntentos = prompt("Si tu rango es +10, te recomendamos escoger más intentos, ¿cuantos necesitas?");
 while (numeroAdivinar != numeroSecreto) {
-  numeroAdivinar = parseInt(prompt("Ingresa un numero del 1 - 10"));
-  
+  numeroAdivinar = parseInt(prompt(`Ingresa un numero entre uno y tu rango maximo`));
   if (numeroAdivinar == numeroSecreto) {
-    alert(`Has acertado, numero secreto: ${numeroSecreto}. Has intentado ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    alert(`Has acertado, numero secreto: ${numeroSecreto}, has intentado ${intentos} ${intentos == 1 ? ' vez' : ' veces'}`);
+  } else if (numeroAdivinar < numeroSecreto) {
+    alert(`El numero es mayor al ingresado, numero ingresado: ${numeroAdivinar}`);
   } else {
-    if (numeroAdivinar < numeroSecreto) {
-      alert(`El numero es mayor al ingresado, numero ingresado: ${numeroAdivinar}`);
-    } else {
-      alert(`El numero es menor al ingresado, numero ingresado:${numeroAdivinar}`);
-    }
-    //se incrementa el contador de veces para el acierto
-    intentos++;
-    //limitar intentos
-    if(intentos > 3){
-      alert("¡Has llegado al numero maximo de intentos!");
-      break;
-    }
+    alert(`El numero es menor al ingresado, numero ingresado: ${numeroAdivinar}`);
   }
+  intentos++;
+  if (intentos > personalizarIntentos) {
+    alert("¡Has llegado al numero maximo de intentos!");
+    break;
+  }
+  
 }
